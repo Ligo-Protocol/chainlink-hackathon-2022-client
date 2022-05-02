73b8493f878725507acdc86d8ee3042dd2539622
Cody Hatfield <cody.hatfield@me.com>
2022-05-01 18:07:10 -0700

    Integrate Smartcar Connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Smartcar from '@smartcar/auth';
+
+async function onComplete(err: any, code: any, status: any) {
+  console.log(process.env.REACT_APP_LIGO_NODE_ENDPOINT)
+  const resp = await axios.post(`${process.env.REACT_APP_LIGO_NODE_ENDPOINT}/api/v0/smartcar/authorize`, {code: code})
+  console.log(resp)
+}
 
 function App() {
+  const smartcar = new Smartcar({
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+    scope: ['required:read_vehicle_info'],
+    testMode: true,
+    onComplete: onComplete,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {
+          smartcar.openDialog({forcePrompt: true});
+        }}>Connect To Smartcar</button>
       </header>
     </div>
   );
